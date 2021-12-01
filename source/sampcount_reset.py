2eaa15b1578a24362cf435da87a829a29a0f5b3b
ErranSousa <sousae@uri.edu>
2021-12-01 11:23:57 -0500

    Code Cleanup and Formatting

--- source/sampcount_reset.py
+++ source/sampcount_reset.py
@@ -2,9 +2,18 @@ import pickle
 import sys
 import os
 
+pickle_file_name = "/home/pi/Documents/Minion_scripts/sampcount.pkl"
+
+msg1 = "Sample Counter Reset Utility  "
+
+print("-"*len(msg1))
+print(msg1)
+print("-"*len(msg1))
+
+
 try:
     #Read out the current value
-    countp = open("/home/pi/Documents/Minion_scripts/sampcount.pkl","rb")
+    countp = open(pickle_file_name,"rb")
     sampcount = pickle.load(countp)
     print("sampcount current value: " + str(sampcount))
     countp.close()
@@ -13,14 +22,14 @@ except:
 
 
 #Reset the value to zero
-countp = open("sampcount.pkl","wb")
-print("Resetting the Sample Counter to 0")
+countp = open(pickle_file_name,"wb")
+print("Resetting the Sample Counter to 0...")
 sampcount = 0
 pickle.dump(sampcount, countp)
 countp.close()
 
 #Verify
-countp = open("/home/pi/Documents/Minion_scripts/sampcount.pkl","rb")
+countp = open(pickle_file_name,"rb")
 sampcount_verify = pickle.load(countp)
 countp.close()
 print("Verify sampcount: " + str(sampcount_verify))
