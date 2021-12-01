cab1cda4c0ab01eb5d6a1a60fe08524eb2b601a8
ErranSousa <sousae@uri.edu>
2021-12-01 11:31:51 -0500

    Add: Code to set the Final Sample Status Flag to False in final_samp_status.pkl

--- source/web_iface/new_mission.py
+++ source/web_iface/new_mission.py
@@ -1,10 +1,24 @@
 import os
 import pickle
 
+#print("\n\rClearing the Sample Counter...")
 countp = open("/home/pi/Documents/Minion_scripts/sampcount.pkl","wb")
 sampcount = 0
 pickle.dump(sampcount, countp)
 countp.close()
+
+#Sets the Final Sample Status Flag to False
+#    False : Final Samples Not Performed
+#    True  : Final Samples Were Performed
+try:
+    print("\n\rSetting Final Sample Status Flag to False...")
+    final_samp_status_fid = open("/home/pi/Documents/Minion_scripts/final_samp_status.pkl","wb")
+    final_sample_status_flag = False
+    pickle.dump(final_sample_status_flag, final_samp_status_fid)
+    final_samp_status_fid.close()
+    print("Done")
+except:
+    print("Could Not Set the Final Sample Status Flag to False")
 
 time = os.popen('ls /home/pi/Desktop/minion_data/INI/1-*.txt').read()
 
@@ -12,7 +26,7 @@ time = time.strip('/home/pi/Desktop/minion_data/INI/1-')
 
 time = time.strip('_TEMPPRES-INI.txt\n')
 
-num_mem = int(len(os.listdir('/home/pi/Desktop/minion_memory/'))) + 1
+num_mem = int(len(os.listdir("/home/pi/Desktop/minion_memory/"))) + 1
 
 save_dir = '/home/pi/Desktop/minion_memory/{}-{}'.format(num_mem, time)
 
