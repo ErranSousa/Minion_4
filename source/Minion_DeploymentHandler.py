6fb88cb0946cfff07cc48ea539d0e27fb1128529
ErranSousa <sousae@uri.edu>
2022-05-02 14:16:31 -0400

    Change Extended_Sampler.py to Initial_Sampler.py

--- source/Minion_DeploymentHandler.py
+++ source/Minion_DeploymentHandler.py
@@ -172,7 +172,7 @@ ping_google = "ping google.com -c 1"
 ps_test = "pgrep -a python"
 
 scriptNames = ["TempPres.py", "Minion_image.py","Minion_image_IF.py", \
-               "OXYBASE_RS232.py","ACC_100Hz.py","Extended_Sampler.py", \
+               "OXYBASE_RS232.py","ACC_100Hz.py","Initial_Sampler.py", \
                "Recovery_Sampler_Burn.py","TempPres_IF.py","OXYBASE_RS232_IF.py", \
                "ACC_100Hz_IF.py","Iridium_gps.py","Iridium_data.py", \
                "xmt_minion_data.py"]
@@ -180,7 +180,7 @@ scriptNames = ["TempPres.py", "Minion_image.py","Minion_image_IF.py", \
 if __name__ == '__main__':
 
     if samp_count == 0:
-        os.system('sudo python3 /home/pi/Documents/Minion_scripts/Extended_Sampler.py &')
+        os.system('sudo python3 /home/pi/Documents/Minion_scripts/Initial_Sampler.py &')
 
     elif samp_count >= TotalSamples + 1 or Abort == True:
         GPIO.output(IO328, 0)
