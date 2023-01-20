906bbce3b8f1c2627d8be283a714d85f1e6fee48
ErranSousa <sousae@uri.edu>
2023-01-20 10:33:21 -0500

    First Commit

---
+++ source/RTC_sync_rpi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# This script should be used to set the Raspberry Pi Time based on the DS3231
+# It also sychronizes the RPI time to the new time
+
+import sys
+sys.path.insert(0, '/home/pi/Documents/Minion_tools/')
+from minion_toolbox import MinionToolbox
+
+# create an instance of MinionToolbox()
+minion_tools = MinionToolbox()
+
+# Synchronize the Raspberry Pi time to the DS3231 RTC.
+minion_tools.rtc_ext_sync_rpi()
