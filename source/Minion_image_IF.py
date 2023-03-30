c9b6f750c68077a9c7fd34e4c6c1dc88fa458df6
ErranSousa <sousae@uri.edu>
2023-03-30 15:02:49 -0400

    Update to use MinionToolbox() features

--- source/Minion_image_IF.py
+++ source/Minion_image_IF.py
@@ -1,109 +1,99 @@
 #!/usr/bin/python3
 from picamera import PiCamera
-import RPi.GPIO as GPIO
 import time
 import os
-import configparser
-import pickle
 import sys
 sys.path.insert(0, '/home/pi/Documents/Minion_tools/')
 from minion_toolbox import MinionToolbox
 
-#data_rec = 16
-
 NumSamples = 0
 samp_count = 1
-
-light = 12
-power = 32
 
 sampNum = [0]
 
 camera = PiCamera()
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(light, GPIO.OUT)
-GPIO.setup(power, GPIO.OUT)
 
 # create an instance of MinionToolbox()
 minion_tools = MinionToolbox()
 
-data_config = configparser.ConfigParser()
-data_config.read('/home/pi/Documents/Minion_scripts/Data_config.ini')
+# Get a dictionary of the pin definitions and an instance of RPi-GPIO
+GPIO, pin_defs_dict = minion_tools.config_gpio()
 
-configDir = data_config['Data_Dir']['Directory']
-configLoc = '{}/Minion_config.ini'.format(configDir)
-config = configparser.ConfigParser()
-config.read(configLoc)
+# Load the Minion Configuration
+minion_mission_config = minion_tools.read_mission_config()
 
-if len(os.listdir('{}/minion_pics'.format(configDir))) == 0:
+# Load the Data Configuration Directory
+data_config = minion_tools.read_data_config()
 
-    Stime = float(config['Initial_Samples']['hours'])
-    Srate = float(config['Initial_Samples']['Camera_sample_rate'])
+# Minion Mission Configuration file
+configLoc = '{}/Minion_config.ini'.format(data_config['Data_Dir'])
+
+# Initial Samples will be performed if pictures have not been performed during Time Lapse mode
+if len(os.listdir('{}/minion_pics'.format(data_config['Data_Dir']))) == 0:
+
+    Srate = minion_mission_config['FINsamp_camera_rate']
+    TotalSamples = minion_mission_config['INIsamp_hours']*(60/minion_mission_config['INIsamp_camera_rate'])
+    # Srate = float(config['Initial_Samples']['Camera_sample_rate'])
     RECOVER = 0
+    mode = 'Initial'
 
-    for dataNum in os.listdir('{}/minion_data/INI/'.format(configDir)):
+    for dataNum in os.listdir('{}/minion_data/INI/'.format(data_config['Data_Dir'])):
         if dataNum.endswith('_Pic-INI.jpg'):
-            sampNum.append(int(dataNum.split("-",1)[0]))
+            sampNum.append(int(dataNum.split("-", 1)[0]))
 
+# Final Samples will be performed
 else:
 
-    Stime = float(config['Final_Samples']['hours'])
-    Srate = float(config['Final_Samples']['Camera_sample_rate'])
+    Srate = minion_mission_config['FINsamp_camera_rate']
+    TotalSamples = minion_mission_config['FINsamp_hours'] * (60 / Srate)
     RECOVER = 1
+    mode = 'Final'
 
-    for dataNum in os.listdir('{}/minion_data/FIN/'.format(configDir)):
+    for dataNum in os.listdir('{}/minion_data/FIN/'.format(data_config['Data_Dir'])):
         if dataNum.endswith('_Pic-FIN.jpg'):
-            sampNum.append(int(dataNum.split("-",1)[0]))
+            sampNum.append(int(dataNum.split("-", 1)[0]))
 
 samp_count = max(sampNum) + 1    
 
-TotalSamples = Stime*(60/Srate)
 
-
-def picture(configDir, NumSamples, RECOVER):
+def picture(config_dir, num_samples, samp_mode):
     try:
-        # with open("/home/pi/Documents/Minion_scripts/timesamp.pkl","rb") as firstp:
-        #     samp_time = pickle.load(firstp)
+        print("Mode: " + mode)
 
         # Update the time stamp
         samp_time = minion_tools.update_timestamp()  # Use when DS3231 is not enabled in config.txt
         print('Time Stamp: ' + str(samp_time))
 
-        GPIO.output(light, 1)
+        GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.HIGH)
         camera.resolution = (2592, 1944)
         camera.framerate = 15
         camera.start_preview()
         time.sleep(10)
 
-        if RECOVER == 0:
+        if samp_mode == 'Initial':
+            samp_time = "{}-{}-{}".format(samp_count, num_samples, samp_time)
+            camera.capture('{}/minion_data/INI/{}_Pic-INI.jpg'.format(config_dir, samp_time))
 
-            samp_time = "{}-{}-{}".format(samp_count, NumSamples, samp_time)
-            camera.capture('{}/minion_data/INI/{}_Pic-INI.jpg'.format(configDir, samp_time))
-
-        else:      
-
-            samp_time = "{}-{}-{}".format(samp_count, NumSamples, samp_time)
-            camera.capture('{}/minion_data/FIN/{}_Pic-FIN.jpg'.format(configDir, samp_time))
+        if samp_mode == 'Final':
+            samp_time = "{}-{}-{}".format(samp_count, num_samples, samp_time)
+            camera.capture('{}/minion_data/FIN/{}_Pic-FIN.jpg'.format(config_dir, samp_time))
         
         time.sleep(1)
         print("Image : {}".format(samp_time))
         camera.stop_preview()
-        GPIO.output(light, 0)
+        GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.LOW)
 
     except:
         camera.stop_preview()
-        GPIO.output(light, 0)
+        GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.LOW)
         print("Camera Error")
 
 
-
-# Spew readings
 while NumSamples <= TotalSamples:
 
     tic = time.perf_counter()
 
-    picture(configDir, NumSamples, RECOVER)
+    picture(data_config['Data_Dir'], NumSamples, mode)
 
     NumSamples = NumSamples + 1
 
@@ -116,4 +106,3 @@ while NumSamples <= TotalSamples:
         timeS = Srate
 
     time.sleep((Srate*60) - timeS)
-
