38363677ddf94f6e3ad81a81cbc71ab919299eb6
ErranSousa <sousae@uri.edu>
2023-03-30 15:07:27 -0400

    Add resetting of burn wire at mission start

--- source/web_iface/new_mission.py
+++ source/web_iface/new_mission.py
@@ -3,32 +3,34 @@ import pickle
 import sys #for sys.path
 sys.path.insert(0,'/home/pi/Documents/Minion_tools/')
 from minion_toolbox import MinionToolbox
-
-# #print("\n\rClearing the Sample Counter...")
-# with open("/home/pi/Documents/Minion_scripts/sampcount.pkl","wb") as countp:
-#     sampcount = 0
-#     pickle.dump(sampcount, countp)
+from minion_hat import MinionHat
 
 #Sets the Final Sample Status Flag to False
 #    False : Final Samples Not Performed
 #    True  : Final Samples Were Performed
 try:
-    print("\n\rSetting Final Sample Status Flag to False...")
     with open("/home/pi/Documents/Minion_scripts/final_samp_status.pkl","wb") as final_samp_status_fid:
         final_sample_status_flag = False
         pickle.dump(final_sample_status_flag, final_samp_status_fid)
-    print("Done")
+    print("[OK] Set Final Sample Status Flag to False")
 except:
-    print("Could Not Set the Final Sample Status Flag to False")
+    print("[Error] Could Not Set the Final Sample Status Flag to False")
 
 # create an instance of MinionToolbox() called minion_tools
 minion_tools = MinionToolbox()
+
+# Create an instance of MinionHat()
+minion_hat = MinionHat()
 
 # Delete the Data Transmit Status Pickle prior to mission start
 minion_tools.delete_data_xmt_status_pickle()
 
 # Delete the Sample Number Pickle (samp_num.pkl) prior to mission start
 minion_tools.delete_samp_num_pickle()
+
+# Reset the Burn Wire
+minion_hat.burn_wire(minion_hat.DISABLE)
+print('[OK] Disabled Burn Wire')
 
 time = os.popen('ls /home/pi/Desktop/minion_data/INI/1-*.txt').read()
 
@@ -52,4 +54,4 @@ os.system('sudo mkdir /home/pi/Desktop/minion_data/INI/ /home/pi/Desktop/minion_
 
 dir_name = save_dir.strip('/home/pi/Desktop/minion_memory/')
 
-print('Files are saved to {}'.format(dir_name))
+print('[OK] Files are saved to {}'.format(dir_name))
