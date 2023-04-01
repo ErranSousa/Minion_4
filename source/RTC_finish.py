94071224306af104af19b09018c5f88e59401e29
ErranSousa <sousae@uri.edu>
2023-04-01 09:39:32 -0400

    Change refernce to RTC_finish_2.py to RTC_finish.py, remove Keep_Me_Alive.py

--- source/RTC_finish.py
+++ source/RTC_finish.py
@@ -23,8 +23,6 @@ os.system(
     "sudo sed -i '/# Print the IP/isudo python /home/pi/Documents/Minion_scripts/Minion_DeploymentHandler.py &\n' /etc/rc.local")
 os.system(
     "sudo sed -i '/# Print the IP/i#sudo python /home/pi/Documents/Minion_scripts/Gelcam_DeploymentHandler.py &\n' /etc/rc.local")
-os.system(
-    "sudo sed -i '/# Print the IP/i#sudo python /home/pi/Documents/Minion_scripts/Keep_Me_Alive.py \n' /etc/rc.local")
 
 # Remove self from rc.local and configure deployment
 
@@ -33,7 +31,7 @@ with open('/etc/rc.local', 'r') as file:
     rclocal = file.read()
 
 # Replace the RTC string
-rclocal = rclocal.replace('sudo python /home/pi/Documents/Minion_tools/RTC_finish_2.py', '')
+rclocal = rclocal.replace('sudo python /home/pi/Documents/Minion_tools/RTC_finish.py', '')
 
 # Write the file out again
 with open('/etc/rc.local', 'w') as file:
