c7607fb7bb4c30c0145e9771be7e9642d65d0d3f
ErranSousa <sousae@uri.edu>
2023-04-07 11:39:55 -0400

    Remove adxl345 installation

--- Minion_install.py
+++ Minion_install.py
@@ -222,14 +222,14 @@ os.chdir('/home/pi/Documents/drivers/')
 os.system('git clone https://github.com/melissaomand/tsys01-python.git')
 os.system('git clone https://github.com/melissaomand/ms5837-python.git')
 os.system('git clone https://github.com/melissaomand/KellerLD-python.git')
-os.system('git clone https://github.com/melissaomand/adxl345-python.git')
+# os.system('git clone https://github.com/melissaomand/adxl345-python.git')
 # os.system('git clone https://github.com/melissaomand/Adafruit_Python_ADS1x15.git')
 
 # # Install adc driver
 # os.chdir('Adafruit_Python_ADS1x15/')
 # os.system('sudo python setup.py install')
 
-os.system('sudo cp /home/pi/Documents/drivers/adxl345-python/adxl345.py /home/pi/Documents/Minion_scripts/')
+# os.system('sudo cp /home/pi/Documents/drivers/adxl345-python/adxl345.py /home/pi/Documents/Minion_scripts/')
 os.system('sudo cp /home/pi/Documents/drivers/KellerLD-python/kellerLD.py /home/pi/Documents/Minion_scripts/')
 os.system('sudo cp /home/pi/Documents/drivers/ms5837-python/ms5837/ms5837.py /home/pi/Documents/Minion_scripts/')
 os.system('sudo cp -r /home/pi/Documents/drivers/tsys01-python/tsys01 /home/pi/Documents/Minion_scripts/')
