39ba4383dd49729ee2d9d8f74d2eaee6a92f4d40
ErranSousa <sousae@uri.edu>
2023-04-10 16:34:55 -0400

    Fix several variables that had conflicts in scope, formatting.

--- source/web_iface/ziparchives.py
+++ source/web_iface/ziparchives.py
@@ -5,45 +5,48 @@ import sys
 
 data = sys.argv[1]
 
-if data == False:
+if data:
     exit(0)
 
 path = '/home/pi/Desktop/minion_memory/{}'.format(data)
 
+
 def get_all_files(directory):
 
-    paths = []
+    file_paths = []
 
     for root, directories, files in os.walk(directory):
-        for filename in files:
-            filepath = os.path.join(root,filename)
-            paths.append(filepath)
+        for fname in files:
+            filepath = os.path.join(root, fname)
+            file_paths.append(filepath)
 
-    return paths
+    return file_paths
+
 
 def clear_log():
-    with open('/var/www/html/livefeed.txt','w') as output:
-        output.write('')
+    with open('/var/www/html/livefeed.txt', 'w') as output_file:
+        output_file.write('')
 
-endmessage = "Files Compressed!"
+
+end_message = "Files Compressed!"
 
 clear_log()
 
-zipname = "/var/www/html/MXXX-{}.zip".format(data)
+zip_name = "/var/www/html/MXXX-{}.zip".format(data)
 
 paths = get_all_files(path)
 
-with ZipFile(zipname,'w') as zip:
+with ZipFile(zip_name, 'w') as archive:
     for file in paths:
-        filename = file.replace('/home/pi/Desktop/minion_memory','')
-        zip.write(file, filename)
-        with open('/var/www/html/livefeed.txt','a+') as output:
+        filename = file.replace('/home/pi/Desktop/minion_memory', '')
+        archive.write(file, filename)
+        with open('/var/www/html/livefeed.txt', 'a+') as output:
             output.write(file)
             output.write('<br><br>')
             output.close()
 
-with open('/var/www/html/livefeed.txt','w') as output:
-    output.write(endmessage)
+with open('/var/www/html/livefeed.txt', 'w') as output:
+    output.write(end_message)
     output.write("<br><br>")
 
 time.sleep(10)
