0eb252923638bba56dea92668dfec2e1d7f72a03
ErranSousa <sousae@uri.edu>
2023-04-12 11:38:09 -0400

    Remove old style gpio setup, remove flash() which was not used, remove unused imports, use MinionToolbox to read the mission configuration.

--- source/web_iface/Image_test.py
+++ source/web_iface/Image_test.py
@@ -1,52 +1,45 @@
 #!/usr/bin/env python
 
 from picamera import PiCamera
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import os
-import configparser
+# import configparser
+import sys
+sys.path.insert(0, '/home/pi/Documents/Minion_tools/')
+from minion_toolbox import MinionToolbox
 
 test_image = '/home/pi/testimage.jpg'
 
-data_config = configparser.ConfigParser()
-data_config.read('/home/pi/Documents/Minion_scripts/Data_config.ini')
+# create an instance of MinionToolbox()
+minion_tools = MinionToolbox()
 
-configDir = data_config['Data_Dir']['Directory']
-configLoc = '{}/Minion_config.ini'.format(configDir)
-config = configparser.ConfigParser()
-config.read(configLoc)
+# Get a dictionary of the pin definitions and an instance of RPi-GPIO
+GPIO, pin_defs_dict = minion_tools.config_gpio()
 
-mNumber = config['MINION']['Number']
-print(mNumber)
+# Load the Minion Configuration
+minion_mission_config = minion_tools.read_mission_config()
 
-GPIO.setwarnings(False)
-
-i = 0
-light = 12
-power = 32
-
-def flash():
-    j = 0
-    while j <= 1:
-        GPIO.output(light, 1)
-        time.sleep(.25)
-        GPIO.output(light, 0)
-        time.sleep(.25)
-        j = j + 1
+# Display the Minion Serial Number
+print('Minion Serial Number: {}'.format(minion_mission_config['Minion_ID']))
 
 
 def picture():
     try:
         # Collect time value from pickle on desktop
-        GPIO.output(light, 1)
+        GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.HIGH)
         camera.resolution = (2592, 1944)
         camera.framerate = 15
         camera.start_preview()
         time.sleep(10)
         camera.capture(test_image)
         time.sleep(5)
+        camera.stop_preview()
+        GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.LOW)
     except:
         print("Camera error")
+        camera.stop_preview()
+        GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.LOW)
 
 
 if __name__ == '__main__':
@@ -57,10 +50,4 @@ if __name__ == '__main__':
     except FileNotFoundError:
         pass
     camera = PiCamera()
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(light, GPIO.OUT)
-    GPIO.setup(power, GPIO.OUT)
     picture()
-    camera.stop_preview()
-    GPIO.output(light, 0)
-
