5ee944e2de4761a4b3b762447f03da2642e5cc68
ErranSousa <sousae@uri.edu>
2023-04-13 09:52:56 -0400

    Update to use MinionHat() class, remove old gpio code.

--- source/Shutdown.py
+++ source/Shutdown.py
@@ -1,15 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import RPi.GPIO as GPIO
-import time
-import os
+import sys
+sys.path.insert(0, '/home/pi/Documents/Minion_tools/')
+from minion_hat import MinionHat
 
-GPIO.setwarnings(False)
+# Create and instance of MinionHat()
+minion_hat = MinionHat()
 
-wifi=22
+# shutdown for a very long time...
+num_years = 1
+shutdown_secs = num_years * 8760 * 60
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(wifi, GPIO.OUT)
-GPIO.output(wifi, 0)
-time.sleep(4)
-os.system("sudo shutdown now")
+res = ''
+while res not in {"y", "n"}:
+    res = input("Confirm Shutdown? (Enter y/n)").lower()
+
+if res == 'y':
+    print('Shutting Down Now.')
+    # Turn on the LED controlled by the Minion Hat microcontroller.
+    # Once this LED is turned off, the user can attach the magnet to keep the minion off.
+    minion_hat.led(minion_hat.ON)
+    minion_hat.shutdown(shutdown_secs)
+else:
+    print('Shutdown canceled.')
+
+
+
+
+
+
