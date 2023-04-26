a880f6fa065592ee7b367cc4d4dd922b88ebbd55
ErranSousa <sousae@uri.edu>
2023-04-26 14:29:21 -0400

    Remove Srate that refers to minion_sleep_cycle

--- source/TempPres.py
+++ source/TempPres.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as GPIO
 import tsys01
 import ms5837
 from kellerLD import KellerLD
 import time
 import os
-import configparser
-import pickle
 import sys
 sys.path.insert(0, '/home/pi/Documents/Minion_tools/')
 from minion_toolbox import MinionToolbox
@@ -18,10 +15,9 @@ NumSamples = 0
 
 samp_count = 1
 
+
 def abort_mission():
-
-#    kill_sampling(scriptNames)
-
+    #  kill_sampling(scriptNames)
     print("Max Depth Exceeded!")
     minion_tools.write_mission_config_option('Mission', 'Abort', '1')
     # abortConfig = configparser.ConfigParser()
@@ -51,20 +47,12 @@ samp_time = minion_tools.read_timestamp()  # Use when DS3231 is not enabled in c
 # Minion Mission Configuration file
 configLoc = '{}/Minion_config.ini'.format(data_config['Data_Dir'])
 
-# scriptNames = ["Minion_image.py", "Minion_image_IF.py", "OXYBASE_RS232.py", "ACC_100Hz.py",
-#                "Recovery_Sampler_Burn.py", "OXYBASE_RS232_IF.py", "ACC_100Hz_IF.py",
-#                "Iridium_gps.py", "Iridium_data.py"]
-
 scriptNames = ["Minion_image.py", "Minion_image_IF.py", "OXYBASE_RS232.py",
                "Recovery_Sampler_Burn.py", "OXYBASE_RS232_IF.py"]
 
 Sf = 1/minion_mission_config['TLPsamp_tempPress_rate']
 
-# TotalSamples = minion_mission_config['TLPsamp_burst_minutes'] * 60 * minion_mission_config['Srate']
 TotalSamples = minion_mission_config['TLPsamp_burst_minutes'] * 60 * minion_mission_config['TLPsamp_tempPress_rate']
-
-# with open("/home/pi/Documents/Minion_scripts/timesamp.pkl","rb") as firstp:
-#     samp_time = pickle.load(firstp)
 
 for dataNum in os.listdir('{}/minion_data/'.format(data_config['Data_Dir'])):
     if dataNum.endswith('_TEMPPRES.txt'):
@@ -72,14 +60,14 @@ for dataNum in os.listdir('{}/minion_data/'.format(data_config['Data_Dir'])):
 
 samp_count_leading_zeros = "%03d" % samp_count
 
-samp_time = "{}-{}".format(samp_count_leading_zeros, samp_time) #Add leading zeros to sample count
+samp_time = "{}-{}".format(samp_count_leading_zeros, samp_time)  # Add leading zeros to sample count
 
 file_name = "{}_TEMPPRES.txt".format(samp_time)
 file_path_name = "{}/minion_data/".format(data_config['Data_Dir']) + file_name
 
 if minion_mission_config['iniP30']:
 
-    Psensor = ms5837.MS5837_30BA() # Default I2C bus is 1 (Raspberry Pi 3)
+    Psensor = ms5837.MS5837_30BA()  # Default I2C bus is 1 (Raspberry Pi 3)
 
     if not Psensor.init():
         print("Failed to initialize P30 sensor!")
@@ -121,18 +109,13 @@ if minion_mission_config['iniTmp']:
         exit(1)
 
 
-# minion_tools.write_data_file_header(DATA_TYPE, file_path_name, file_name, minion_mission_config['Srate'],
-#                                     minion_mission_config['iniP30'], minion_mission_config['iniP100'],
-#                                     minion_mission_config['iniTmp'])
-
 minion_tools.write_data_file_header(DATA_TYPE, file_path_name, file_name,
                                     minion_mission_config['TLPsamp_tempPress_rate'],
                                     minion_mission_config['iniP30'],
                                     minion_mission_config['iniP100'],
                                     minion_mission_config['iniTmp'])
 
-# Spew readings
-# while NumSamples <= TotalSamples:
+# Display Readings
 while NumSamples < TotalSamples:
 
     tic = time.perf_counter()
@@ -142,11 +125,12 @@ while NumSamples < TotalSamples:
 
     sensor_string = ''
 
-    if minion_mission_config['iniP100'] or minion_mission_config['iniP30'] == True:
+    if minion_mission_config['iniP100'] or minion_mission_config['iniP30']:
 
         if Psensor.read():
-            Ppressure = round((Psensor.pressure() * depth_factor) - surface_offset, 3)*1000  # shifting the decimal point out
-            Ppressure = "%07d" % Ppressure  #fixed field / prepending zeros
+            # shifting the decimal point out
+            Ppressure = round((Psensor.pressure() * depth_factor) - surface_offset, 3)*1000
+            Ppressure = "%07d" % Ppressure  # fixed field / prepending zeros
             Ptemperature = round(Psensor.temperature(), 2)*100  # shifting the decimal point out
             Ptemperature = "%04d" % Ptemperature  # fix field length by prepending zeros if necessary
             Pres_data = "{},{},".format(Ppressure, Ptemperature)
@@ -159,23 +143,21 @@ while NumSamples < TotalSamples:
                 file.write('Pressure Sensor fail')
             abort_mission()
 
-        #if Ppressure >= MAX_Depth:
         if int(Ppressure)/1000 >= minion_mission_config['MAX_Depth']:
             with open(file_path_name, "a") as file:
                 file.write("Minion Exceeded Depth Maximum!")
             abort_mission()
 
-
-    if minion_mission_config['iniTmp'] == True:
+    if minion_mission_config['iniTmp']:
 
         if not sensor_temp.read():
             print("Error reading sensor")
             minion_mission_config['iniTmp'] = False
 
-        Temp_acc = round(sensor_temp.temperature(),2)*100
+        Temp_acc = round(sensor_temp.temperature(), 2)*100
         Temp_acc = "%04d" % Temp_acc  # fix field length by prepending zeros if necessary
 
-        print("Temperature_accurate: {} C*100".format(Temp_acc)) #degrees Celsius * 100
+        print("Temperature_accurate: {} C*100".format(Temp_acc))  # degrees Celsius * 100
 
         sensor_string = '{}{}'.format(sensor_string, Temp_acc)
 
@@ -193,7 +175,3 @@ while NumSamples < TotalSamples:
         timeS = Sf
 
     time.sleep(Sf - timeS)
-
-
-
-
