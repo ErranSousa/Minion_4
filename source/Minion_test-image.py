d745de483b2d442f421e935c35725c88dfbb6df5
ErranSousa <sousae@uri.edu>
2023-04-27 09:33:41 -0400

    Overhauled

--- source/Minion_test-image.py
+++ source/Minion_test-image.py
@@ -1,58 +1,39 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from picamera import PiCamera
-import RPi.GPIO as GPIO
 import time
-import os
-import configparser
-
-data_config = configparser.ConfigParser()
-data_config.read('/home/pi/Documents/Minion_scripts/Data_config.ini')
-
-configDir = data_config['Data_Dir']['Directory']
-configLoc = '{}/Minion_config.ini'.format(configDir)
-config = configparser.ConfigParser()
-config.read(configLoc)
-
-mNumber = config['MINION']['Number']
-
-print(mNumber)
-
-GPIO.setwarnings(False)
-
-i = 0
-light = 12
-power = 32
-
-def flash():
-    j = 0
-    while j <= 1:
-        GPIO.output(light, 1)
-        time.sleep(.25)
-        GPIO.output(light, 0)
-        time.sleep(.25)
-        j = j + 1
+import sys
+sys.path.insert(0, '/home/pi/Documents/Minion_tools/')
+from minion_toolbox import MinionToolbox
 
 
 def picture():
     try:
-        # Collect time value from pickle on desktop
-        GPIO.output(light, 1)
         camera.resolution = (2592, 1944)
         camera.framerate = 15
         camera.start_preview()
         time.sleep(10)
-        camera.capture('/home/pi/M{}-test.jpg'.format(mNumber))
+        camera.capture('/home/pi/M{}-test.jpg'.format(minion_mission_config['Minion_ID']))
         time.sleep(5)
+        camera.stop_preview()
     except:
         print("Camera error")
+
+
+# create an instance of MinionToolbox()
+minion_tools = MinionToolbox()
+
+# Get a dictionary of the pin definitions and an instance of RPi-GPIO
+GPIO, pin_defs_dict = minion_tools.config_gpio()
+
+# Load the Minion Configuration
+minion_mission_config = minion_tools.read_mission_config()
+
+print("Minion Serial Number: {}".format(minion_mission_config['Minion_ID']))
+
 
 if __name__ == '__main__':
 
     camera = PiCamera()
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(light, GPIO.OUT)
-    GPIO.setup(power, GPIO.OUT)
+    GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.HIGH)
     picture()
-    camera.stop_preview()
-    GPIO.output(light, 0)
-
+    GPIO.output(pin_defs_dict['LED_RING_CTRL'], GPIO.LOW)
