7fa8e6fd93f3692b2ca526bdaba274de3762e382
ErranSousa <sousae@uri.edu>
2023-04-28 11:30:44 -0400

    Update for Minion_4 & python3

--- source/RTC_finish.py
+++ source/RTC_finish.py
@@ -1,28 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
-
-import RPi.GPIO as GPIO
-
-wifi = 7
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(wifi, GPIO.OUT)
-GPIO.output(wifi, 1)
-
-
-
 
 # Write deployment scripts to rc.local
 os.system(
-    "sudo sed -i '/# Print the IP/i# Synchronize the RPi clock to the external RTC\n' /etc/rc.local")
+    "sudo sed -i '/# Print the IP/i# Synchronize the RPi Clock to the external RTC for every startup\n' /etc/rc.local")
 os.system(
-    "sudo sed -i '/# Print the IP/isudo python /home/pi/Documents/Minion_tools/RTC_sync_rpi.py\n' /etc/rc.local")
+    "sudo sed -i '/# Print the IP/isudo python3 /home/pi/Documents/Minion_tools/RTC_sync_rpi.py\n\n' /etc/rc.local")
 os.system(
-    "sudo sed -i '/# Print the IP/i# Choose one of the following Handlers\n' /etc/rc.local")
+    "sudo sed -i '/# Print the IP/i# Choose one of the following Handlers:\n' /etc/rc.local")
 os.system(
     "sudo sed -i '/# Print the IP/isudo python3 /home/pi/Documents/Minion_scripts/Minion_DeploymentHandler.py &\n' /etc/rc.local")
 os.system(
-    "sudo sed -i '/# Print the IP/i#sudo python /home/pi/Documents/Minion_scripts/Gelcam_DeploymentHandler.py &\n' /etc/rc.local")
+    "sudo sed -i '/# Print the IP/i#sudo python3 /home/pi/Documents/Minion_scripts/Gelcam_DeploymentHandler.py &\n' /etc/rc.local")
 
 # Remove self from rc.local and configure deployment
 
@@ -31,7 +20,7 @@ with open('/etc/rc.local', 'r') as file:
     rclocal = file.read()
 
 # Replace the RTC string
-rclocal = rclocal.replace('sudo python /home/pi/Documents/Minion_tools/RTC_finish.py', '')
+rclocal = rclocal.replace('sudo python3 /home/pi/Documents/Minion_tools/RTC_finish.py', '')
 
 # Write the file out again
 with open('/etc/rc.local', 'w') as file:
