78b59d2cba28ef75cea9e140780e8d513ba1f068
ErranSousa <sousae@uri.edu>
2023-10-13 07:34:24 -0400

    Modify page layout for readability and language consistency, change oxygen sample rate to sample period.

--- source/web_iface/conf_edit.php
+++ source/web_iface/conf_edit.php
@@ -27,10 +27,12 @@ if ($cfg_file['Sampling_scripts']['temperature'] == 1) { $Temp = "checked";} els
 if ($cfg_file['Sampling_scripts']['oxybase'] == 1) { $OXY = "checked";} else { $OXY = "";}
 
 echo '<form action="/action_page.php" method="post">
-  <label for="DMAX">Maximum Depth (meters):</label>
-  <input type="text" id="DMAX" name="DMAX" value="'.$cfg_file['Mission']['max_depth'].'"><br><br>
+  <label for="DMAX">Maximum Depth:</label>
+  <br>
+  <input type="text" id="DMAX" name="DMAX" size="6" style="text-align:right" value="'.$cfg_file['Mission']['max_depth'].'"> meters<br>
+  <br>
   <fieldset>
-  <legend>Sensors</legend>
+  <legend><b>Sensors</b></legend>
   <input '.$Image.' type="checkbox" id="Image" name="Image" value="Image">
   <label for="Image"> Image</label><br>
   <input '.$BA30.' type="checkbox" id="30Bar_Pres" name="30Bar_Pres" value="30Bar_Pres">
@@ -42,61 +44,82 @@ echo '<form action="/action_page.php" method="post">
   <input '.$OXY.' type="checkbox" id="OXY" name="OXY" value="OXY">
   <label for="OXY"> Oxybase O2 Sensor</label><br>
   </fieldset>
-  <fieldset>
-  <legend>Initial Sampling Mode:</legend>
-  <label for="IHours">Initial Sample Time (hours):</label><br>
-  <input type="text" id="IHours" name="IHours" value="'.$cfg_file['Initial_Samples']['hours'].'"><br>
-  <label for="ICamFS">Camera Sample Rate (minutes):</label><br>
-  <input type="text" id="ICamFS" name="ICamFS" value="'.$cfg_file['Initial_Samples']['camera_sample_rate'].'"><br>
-  <label for="ITPFS">Temperature and Pressure Sample Rate (Hz):</label><br>
-  <input type="text" id="ITPFS" name="ITPFS" value="'.$cfg_file['Initial_Samples']['temppres_sample_rate'].'"><br>
-  <label for="IOXYFS">Dissolved Oxygen Sample Rate (Hz):</label><br>
-  <input type="text" id="IOXYFS" name="IOXYFS" value="'.$cfg_file['Initial_Samples']['oxygen_sample_rate'].'"><br>
-  </fieldset>
-  <fieldset>
-  <legend>Time Lapse Sampling Mode:</legend>
-  <label for="THours">Hours:</label><br>
-  <input type="text" id="THours" name="THours" value="'.$cfg_file['Time_Lapse_Samples']['hours'].'"><br>
-  <label for="DS_Time">Sample Burst Duration (min):</label><br>
-  <input type="text" id="DS_Time" name="DS_Time" value="'.$cfg_file['Time_Lapse_Samples']['sample_burst_duration'].'"><br>
-    <label for="DS_Interval">Sample Burst Interval (min):</label><br>
-  <input type="text" id="DS_Interval" name="DS_Interval" value="'.$cfg_file['Time_Lapse_Samples']['sample_interval_minutes'].'"><br>
-  <label for="SensorFS">Temperature and Pressure Sample Rate (Hz):</label><br>
-  <input type="text" id="SensorFS" name="SensorFS" value="'.$cfg_file['Time_Lapse_Samples']['temppres_sample_rate'].'"><br>
-  <label for="OxygenFS">Oxybase Sample Rate (Hz):</label><br>
-  <input type="text" id="OxygenFS" name="OxygenFS" value="'.$cfg_file['Time_Lapse_Samples']['oxygen_sample_rate'].'"><br>
-  </fieldset>
-  <fieldset>
-  <legend>Final Sampling Mode:</legend>
-  <label for="FHours">Final Sample Time (hours):</label><br>
-  <input type="text" id="FHours" name="FHours" value="'.$cfg_file['Final_Samples']['hours'].'"><br>
-  <label for="FCamFS">Camera Sample Rate (minutes):</label><br>
-  <input type="text" id="FCamFS" name="FCamFS" value="'.$cfg_file['Final_Samples']['camera_sample_rate'].'"><br>
-  <label for="FTPFS">Temperature and Pressure Sample Rate (Hz):</label><br>
-  <input type="text" id="FTPFS" name="FTPFS" value="'.$cfg_file['Final_Samples']['temppres_sample_rate'].'"><br>
-  <label for="FOXYFS">Dissolved Oxygen Sample Rate (Hz):</label><br>
-  <input type="text" id="FOXYFS" name="FOXYFS" value="'.$cfg_file['Final_Samples']['oxygen_sample_rate'].'"><br>
-  </fieldset>
-  <fieldset>
-  <legend>GPS Transmission Window Settings:</legend>
-  <label for="gps_dur_hrs">GPS Transmission Window Duration (hours):</label><br>
-  <input type="text" id="gps_dur_hrs" name="gps_dur_hrs" value="'.$cfg_file['GPS']['gps_transmission_window'].'"><br>
-  <label for="gps_interval_min">GPS Position Interval (minutes):</label><br>
-  <input type="text" id="gps_interval_min" name="gps_interval_min" value="'.$cfg_file['GPS']['gps_transmission_interval'].'"><br>
-  </fieldset>
-  <fieldset>
-  <legend>Ignore WIFI Signal:</legend>
   <br>
-  <label for="IG_WIFI-days">Days:</label>
-  <input type="text" id="IG_WIFI-days" name="IG_WIFI-days" value="'.$cfg_file['Mission']['ignore_wifi-days'].'"><br><br>
-  <label for="IG_WIFI-hours">Hours:</label>
-  <input type="text" id="IG_WIFI-hours" name="IG_WIFI-hours" value="'.$cfg_file['Mission']['ignore_wifi-hours'].'"><br>
-  </fieldset>
+  <fieldset>
+  <legend><b>Initial Sampling Mode</b></legend>
+  <br>
+  <label for="IHours">Total Mode Duration:</label><br>
+  <input type="text" id="IHours" name="IHours" size="6" style="text-align:right" value="'.$cfg_file['Initial_Samples']['hours'].'"> hours<br>
+  <br>
+  <label for="ICamFS">Image Capture Period:</label><br>
+  <input type="text" id="ICamFS" name="ICamFS" size="6" style="text-align:right" value="'.$cfg_file['Initial_Samples']['camera_sample_rate'].'"> minutes<br>
+  <br>
+  <label for="ITPFS">Temperature and Pressure Sample Rate:</label><br>
+  <input type="text" id="ITPFS" name="ITPFS" size="6" style="text-align:right" value="'.$cfg_file['Initial_Samples']['temppres_sample_rate'].'"> Hz<br>
+  <br>
+  <label for="IOXYFS">OxyBase Sample Period:</label><br>
+  <input type="text" id="IOXYFS" name="IOXYFS" size="6" style="text-align:right" value="'.$cfg_file['Initial_Samples']['oxygen_sample_period'].'"> seconds<br>
+  <br>
   </fieldset>
   <br>
   <fieldset>
-  <legend>Confirm Minion Number:</legend>
-  <input type="text" id="MNumber" name="MNumber" value="'.$cfg_file['MINION']['number'].'"  required/>
+  <legend><b>Time Lapse Sampling Mode</b></legend>
+  <p style="color:red;"><em>Note: If (Sample Burst Interval - Sample Burst Duration) < 2 minutes, the system will not enter low power mode between sample bursts.</em></p>
+  <label for="THours">Total Mode Duration:</label><br>
+  <input type="text" id="THours" name="THours" size="6" style="text-align:right" value="'.$cfg_file['Time_Lapse_Samples']['hours'].'"> hours<br>
+  <br>
+  <label for="DS_Time">Sample Burst Duration:</label><br>
+  <input type="text" id="DS_Time" name="DS_Time" size="6" style="text-align:right" value="'.$cfg_file['Time_Lapse_Samples']['sample_burst_duration'].'"> minutes<br>
+  <br>
+  <label for="DS_Interval">Sample Burst Interval:</label><br>
+  <input type="text" id="DS_Interval" name="DS_Interval" size="6" style="text-align:right" value="'.$cfg_file['Time_Lapse_Samples']['sample_interval_minutes'].'"> minutes<br>
+  <br>
+  <label for="SensorFS">Temperature and Pressure Sample Rate:</label><br>
+  <input type="text" id="SensorFS" name="SensorFS" size="6" style="text-align:right" value="'.$cfg_file['Time_Lapse_Samples']['temppres_sample_rate'].'"> Hz<br>
+  <br>
+  <label for="OxygenFS">OxyBase Sample Period:</label><br>
+  <input type="text" id="OxygenFS" name="OxygenFS" size="6" style="text-align:right" value="'.$cfg_file['Time_Lapse_Samples']['oxygen_sample_period'].'"> seconds<br>
+  <br>
+  </fieldset>
+  <br>
+  <fieldset>
+  <legend><b>Final Sampling Mode</b></legend>
+  <br>
+  <label for="FHours">Total Mode Duration:</label><br>
+  <input type="text" id="FHours" name="FHours" size="6" style="text-align:right" value="'.$cfg_file['Final_Samples']['hours'].'"> hours<br>
+  <br>
+  <label for="FCamFS">Image Capture Period:</label><br>
+  <input type="text" id="FCamFS" name="FCamFS" size="6" style="text-align:right" value="'.$cfg_file['Final_Samples']['camera_sample_rate'].'"> minutes<br>
+  <br>
+  <label for="FTPFS">Temperature and Pressure Sample Rate:</label><br>
+  <input type="text" id="FTPFS" name="FTPFS" size="6" style="text-align:right" value="'.$cfg_file['Final_Samples']['temppres_sample_rate'].'"> Hz<br>
+  <br>
+  <label for="FOXYFS">OxyBase Sample Period:</label><br>
+  <input type="text" id="FOXYFS" name="FOXYFS" size="6" style="text-align:right" value="'.$cfg_file['Final_Samples']['oxygen_sample_period'].'"> seconds<br>
+  <br>
+  </fieldset>
+  <br>
+  <fieldset>
+  <legend><b>GPS Transmission Window Settings</b></legend>
+  <br>
+  <label for="gps_dur_hrs">GPS Transmission Window Duration:</label><br>
+  <input type="text" id="gps_dur_hrs" name="gps_dur_hrs" size="6" style="text-align:right" value="'.$cfg_file['GPS']['gps_transmission_window'].'"> hours<br>
+  <br>
+  <label for="gps_interval_min">GPS Position Interval:</label><br>
+  <input type="text" id="gps_interval_min" name="gps_interval_min" size="6" style="text-align:right" value="'.$cfg_file['GPS']['gps_transmission_interval'].'"> minutes<br>
+  <br>
+  </fieldset>
+  <br>
+  <fieldset>
+  <legend><b>Ignore WIFI Signal</b></legend>
+  <br>
+  <input type="text" id="IG_WIFI-hours" name="IG_WIFI-hours" size="6" style="text-align:right" value="'.$cfg_file['Mission']['ignore_wifi-hours'].'"> hours<br>
+  <br>
+  </fieldset>
+  <br>
+  <fieldset>
+  <legend><b>Confirm Minion Number</b></legend>
+  <input type="text" id="MNumber" name="MNumber" size="6" style="text-align:right" value="'.$cfg_file['MINION']['number'].'"  required/>
   </fieldset>
   <br>
   <input type="submit" value="Review">
